8dfc44ebdac7efb60c026c6dd008abd580babe1f
ankitkalbhor333 <159504400+ankitkalbhor333@users.noreply.github.com>
2025-09-24 09:08:42 +0530

    Update HeroBanner.jsx

--- frontendapp/src/component/home/HeroBanner.jsx
+++ frontendapp/src/component/home/HeroBanner.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./herobanner.css";
+import "./HeroBanner.css";
 
 const HeroBanner = () => {
   return (
@@ -19,3 +19,4 @@ const HeroBanner = () => {
 };
 
 export default HeroBanner;
+
